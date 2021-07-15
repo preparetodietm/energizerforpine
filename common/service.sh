f802dceb88ddb1e4a9c4b0e59df989fce6e1b133
Mark Gerald Tagle <85426884+preparetodietm@users.noreply.github.com>
2021-07-15 13:19:10 +0800

    Update service.sh

--- common/service.sh
+++ common/service.sh
@@ -20,14 +20,6 @@ if [ -e /sys/class/leds/lcd-backlight/max_brightness ]; then
   echo "4087" > /sys/class/leds/lcd-backlight/max_brightness
 fi
 
-# Remove Flush RAM Logs after Booted
-sleep 90
-
-if [ -f /data/adb/modules/flushram/module.prop ]; then
-  rm -rf "/data/media/0/weareravens.log"
-  rm -rf "/data/media/0/.weareravens/weareravens.log"
-fi
-
 #=========================================================
 # End of service.sh
 # Modified by @preparetodietm
