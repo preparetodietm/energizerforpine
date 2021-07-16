de8584b52e84d78765f91e5136ad6624b6e42e97
Mark Gerald Tagle <85426884+preparetodietm@users.noreply.github.com>
2021-07-17 00:07:20 +0800

    Update service.sh

--- common/service.sh
+++ common/service.sh
@@ -1,6 +1,5 @@
 #!/system/bin/sh
 # This script will be executed in late_start service mode
-# More info in the main Magisk thread
 #=========================================================
 MODDIR=${0%/*}
 
@@ -19,6 +18,32 @@ fi
 if [ -e /sys/class/leds/lcd-backlight/max_brightness ]; then
   echo "4087" > /sys/class/leds/lcd-backlight/max_brightness
 fi
+#1
+#WifiFixes
+#WF_one
+#WF_two
+#WF_three
+#WF_four
+#2
+#CustSwap
+#CS_one
+#CS_two
+#CS_three
+#CS_four
+#CS_five 2>&1
+#CS_six 2>&1
+#CS_seven
+#CS_eight
+#CS_nine
+#CS_ten
+#3
+#FlushRAM
+#FR_one
+#FR_two
+#FR_three
+#FR_four
+#FR_five
+#FR_six
 
 #=========================================================
 # End of service.sh
