711f0ec0d28d16892ac3a2e7a37c24b5942bba13
Mark Gerald Tagle <85426884+preparetodietm@users.noreply.github.com>
2021-07-17 00:06:47 +0800

    Update post-fs-data.sh

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -1,8 +1,12 @@
 #!/system/bin/sh
 # This script will be executed in post-fs-data mode
-# More info in the main Magisk thread
 #=========================================================
 MODDIR=${0%/*}
+#1
+#CustSwap
+#CS_one 2>&1
+#CS_two 2>&1
+#CS_three
 
 #=========================================================
 # End of service.sh
