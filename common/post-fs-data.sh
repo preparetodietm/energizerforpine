e2e03bbefd0877862f978d83bc09058ab494f0b2
Mark Gerald Tagle <85426884+preparetodietm@users.noreply.github.com>
2021-11-04 10:53:25 +0800

    Update post-fs-data.sh
    Removed Cust Swap

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -2,13 +2,8 @@
 # This script will be executed in post-fs-data mode
 #=========================================================
 MODDIR=${0%/*}
-#1
-#CustSwap
-#CS_one 2>&1
-#CS_two 2>&1
-#CS_three
 
 #=========================================================
-# End of service.sh
+# End of post-fs-data.sh
 # Modified by @preparetodietm
 #=========================================================
