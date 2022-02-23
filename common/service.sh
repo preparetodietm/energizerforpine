c7c4bba5c3122361260f29583d0752ce9a1bd8fe
Mark Gerald Tagle <85426884+preparetodietm@users.noreply.github.com>
2022-02-23 10:36:45 +0800

    Update service.sh
    - Fixed wrong permissions
    - Fixed startup blackscreen after splash image
    - Fixed Switch to Portable Interpreter looping prop

--- common/service.sh
+++ common/service.sh
@@ -8,21 +8,14 @@ if [ -e /sys/class/kgsl/kgsl-3d0/throttling ]; then
   echo "0" > /sys/class/kgsl/kgsl-3d0/throttling
 fi
 
-# Disable UBWC for Graphics
-setprop debug.gralloc.gfx_ubwc_disable 1
-
 # Enable fast charging
 if [ -e /sys/kernel/fast_charge/force_fast_charge ]; then
   echo "1" > /sys/kernel/fast_charge/force_fast_charge
 fi
 # by AkiraNoSushi
 
-# Set default brightness
-if [ -e /sys/class/leds/lcd-backlight/max_brightness ]; then
-  echo "4087" > /sys/class/leds/lcd-backlight/max_brightness
-fi
-
 # Switch to Portable Interpreter
+rm -rf /data/local.prop
 echo dalvik.vm.execution-mode = int:portable >> /data/local.prop
 #1
 #WifiFixes
@@ -31,7 +24,7 @@ echo dalvik.vm.execution-mode = int:portable >> /data/local.prop
 #WF_three
 #WF_four
 
-#=========================================================
+#=============================
 # End of service.sh
 # Modified by @preparetodietm
-#=========================================================
+#=============================
